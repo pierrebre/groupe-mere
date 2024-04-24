a2c2ab50b742c8fe00814a8608e57843eb395e39
maxentr <rouaultmaxent@gmail.com>
2024-04-24 15:43:33 +0200

    refactor marque page with job card component

--- web/app/themes/pbTheme/single-marque.php
+++ web/app/themes/pbTheme/single-marque.php
@@ -1,35 +1,11 @@
-<?php get_header(); ?>
+<?php get_header();
 
-<?php if (have_posts()) : ?>
-    <?php while (have_posts()) : the_post(); ?>
-        <section>
-            <div class="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
-                <div class="grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16">
-                    <div class="relative h-64 overflow-hidden rounded-lg sm:h-80 lg:order-last lg:h-full">
-                        <?php the_post_thumbnail('full', ['class' => '']) ?>
-                    </div>
-
-                    <div class="lg:py-24">
-                        <h2 class="text-3xl font-bold sm:text-4xl"><?= the_title() ?></h2>
-
-                        <p class="mt-4 text-gray-600">
-                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut qui hic atque tenetur quis
-                            eius quos ea neque sunt, accusantium soluta minus veniam tempora deserunt? Molestiae eius
-                            quidem quam repellat.
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </section>
-    <?php endwhile; ?>
-<?php endif; ?>
-<?php
 $args = array(
-    'post_type'  => 'job',
+    'post_type' => 'job',
     'meta_query' => array(
         array(
-            'key'     => 'job_marque',
-            'value'   => get_the_ID(),
+            'key' => 'job_marque',
+            'value' => get_the_ID(),
             'compare' => 'LIKE',
         ),
     ),
@@ -37,33 +13,39 @@ $args = array(
 
 $jobs_query = new WP_Query($args);
 
-if ($jobs_query->have_posts()) {
+the_post(); ?>
 
-    while ($jobs_query->have_posts()) {
-        $jobs_query->the_post();
-?>
-        <section>
-            <div class="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
-                <div class="grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16">
-                    <div class="relative h-64 overflow-hidden rounded-lg sm:h-80 lg:order-last lg:h-full">
-                        <?php the_post_thumbnail('full', ['class' => '']) ?>
-                    </div>
+<section class="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
+    <h2 class="mt-8 text-3xl font-bold sm:text-4xl text-center lg:text-start"><?= the_title() ?></h2>
+    <div class="flex flex-col-reverse lg:flex-row mt-4 lg:mt-0 gap-2 items-center justify-between">
 
-                    <div class="lg:py-24">
-                        <h2 class="text-3xl font-bold sm:text-4xl"><?= the_title() ?></h2>
+        <p class="mt-4 text-gray-600 text-justify lg:text-start">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut qui hic atque tenetur quis
+            eius quos ea neque sunt, accusantium soluta minus veniam tempora deserunt? Molestiae eius
+            quidem quam repellat.
+        </p>
 
-                        <a href="<?= get_the_permalink() ?>" class="mt-4 text-blue-600 hover:underline">Voir l'offre</a>
-                    </div>
-                </div>
+        <?php the_post_thumbnail('full', ['class' => '']) ?>
+    </div>
+    <div class="flex flex-col gap-4 mt-12 mb-8">
+        <p class="text-2xl text-center lg:text-start">
+            Les jobs la marque
+        </p>
+        <?php if ($jobs_query->have_posts()) {
+            while ($jobs_query->have_posts()) {
+                $jobs_query->the_post();
+                get_template_part('partials/job-card', null, array('query' => $jobs_query));
+            }
+            wp_reset_postdata();
+        } else { ?>
+            <div class="flex flex-col items-center justify-center gap-4">
+                <p class="text-center text-gray-500">
+                    Aucune offre n'a été trouvée
+                </p>
             </div>
-        </section>
+        <?php } ?>
+    </div>
+</section>
 <?php
-    }
-    wp_reset_postdata();
-} else {
-    // Aucun post trouvé
-    echo 'Aucun post trouvé';
-}
-?>
 
-<?php get_footer(); ?>
+get_footer(); ?>
