ddd96bd2427d18c2f2d50cafbb22154c21ea158f
maxentr <rouaultmaxent@gmail.com>
2024-04-26 11:32:14 +0200

    feat add icon in tag

--- web/app/themes/pbTheme/partials/tag.php
+++ web/app/themes/pbTheme/partials/tag.php
@@ -1,5 +1,5 @@
 <div class="flex flex-row items-center bg-gray-100 rounded px-2 py-1 w-fit">
-  <span class="dashicons dashicons-money-alt text-sm"></span>
+  <span class="dashicons text-sm <?= $args['icon'] ?? ''; ?>"></span>
   <p class="text-xs <?= $args['class'] ?? ''; ?>">
     <?= $args['content']; ?>
   </p>
