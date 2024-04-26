7a2b6f46a28878b6a476893a3ecac0eea5b7a8d1
maxentr <rouaultmaxent@gmail.com>
2024-04-26 10:35:36 +0200

    refactor body style

--- web/app/themes/pbTheme/header.php
+++ web/app/themes/pbTheme/header.php
@@ -19,12 +19,17 @@ use pbTheme\walker\MenuWalker;
                 <div class="relative flex h-16 justify-between">
                     <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
                         <!-- Mobile menu button -->
-                        <button type="button" class="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500" aria-controls="mobile-menu" aria-expanded="false">
+                        <button type="button"
+                            class="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+                            aria-controls="mobile-menu" aria-expanded="false">
                             <span class="sr-only">Ouvrir le menu</span>
-                            <svg class="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                            <svg class="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                stroke="currentColor" aria-hidden="true">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                             </svg>
-                            <svg class="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                            <svg class="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                stroke="currentColor" aria-hidden="true">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
                             </svg>
                         </button>
@@ -35,8 +40,10 @@ use pbTheme\walker\MenuWalker;
                             $image_attributes = wp_get_attachment_image_src(6, '');
                             ?>
                             <a href="<?php echo esc_url(home_url('/')); ?>" class="flex items-center">
-                                <img class="block h-12 w-auto lg:hidden" src="<?php echo esc_url($image_attributes[0]); ?>" alt="logo groupe méré">
-                                <img class="hidden h-12 w-auto lg:block" src="<?php echo esc_url($image_attributes[0]); ?>" alt="logo groupe méré">
+                                <img class="block h-12 w-auto lg:hidden"
+                                    src="<?php echo esc_url($image_attributes[0]); ?>" alt="logo groupe méré">
+                                <img class="hidden h-12 w-auto lg:block"
+                                    src="<?php echo esc_url($image_attributes[0]); ?>" alt="logo groupe méré">
                             </a>
                         </div>
                         <?php
@@ -68,4 +75,4 @@ use pbTheme\walker\MenuWalker;
             </div>
         </nav>
     </header>
-    <div class="mx-auto max-w-screen-xl  px-4 sm:px-6 lg:px-8">
+    <div class="mx-auto w-full max-w-screen-xl px-4 sm:px-6 lg:px-8">
