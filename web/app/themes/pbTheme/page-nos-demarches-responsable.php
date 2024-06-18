d670e81a6c98a7903a1add7718652bffdd7b8648
Pierre Barbé <pierrebarbe0@gmail.com>
2024-06-18 21:05:40 +0200

    feat: Update links in page-le-groupe.php and page-notre-responsabilite-employeur.php
    Update the links in page-le-groupe.php and page-notre-responsabilite-employeur.php to point to the correct pages. This ensures that users are directed to the appropriate content when clicking on the links.
    
    Note: This is a suggested commit message based on the provided code changes. Please review and modify it as needed.

---
+++ web/app/themes/pbTheme/page-nos-demarches-responsable.php
@@ -0,0 +1,60 @@
+<?php get_header(); ?>
+<section class="py-16 lg:min-w-[800px] mx-auto w-fit">
+  <h1 class="text-gray-800 text-xl font-extrabold sm:text-2xl text-center"><?php the_title() ?></h1>
+  <div class="text-gray-800">
+    <h2 class="text-2xl font-bold mt-8">Nos Démarches Responsables</h2>
+    <p class="mt-4">Chez le Groupe Méré, nous croyons en la nécessité d’adopter des pratiques responsables et durables dans toutes nos activités. Nous nous engageons à protéger l’environnement, à réduire notre empreinte carbone et à encourager une consommation responsable. Voici comment nous mettons en œuvre ces engagements au quotidien.</p>
+
+    <h3 class="text-xl font-semibold mt-6">Initiatives Écologiques</h3>
+    <p class="mt-4"><strong>Réduction des Déchets</strong></p>
+    <p class="mt-2">Nous avons mis en place des programmes de réduction des déchets dans tous nos magasins et bureaux. Cela inclut des initiatives telles que :</p>
+    <ul class="list-disc list-inside mt-2">
+      <li><strong>Recyclage Intensif :</strong> Nous recyclons les matériaux comme le papier, le plastique, le métal et le verre. Des points de collecte sont disponibles dans nos installations pour faciliter ce processus.</li>
+      <li><strong>Réduction des Emballages :</strong> Nous travaillons avec nos fournisseurs pour réduire la quantité d'emballage et privilégier les matériaux recyclables ou biodégradables.</li>
+      <li><strong>Matériaux Durables :</strong> Nous favorisons l’utilisation de matériaux durables et respectueux de l’environnement dans nos produits et nos pratiques opérationnelles.</li>
+    </ul>
+
+    <h3 class="text-xl font-semibold mt-6">Réduction de l'Empreinte Carbone</h3>
+    <p class="mt-4"><strong>Optimisation des Transports</strong></p>
+    <p class="mt-2">Nous optimisons nos chaînes d’approvisionnement pour réduire les émissions de carbone associées au transport. Cela comprend :</p>
+    <ul class="list-disc list-inside mt-2">
+      <li><strong>Transport Éco-responsable :</strong> Utilisation de véhicules moins polluants et maximisation du remplissage des camions pour réduire le nombre de trajets nécessaires.</li>
+      <li><strong>Proximité des Fournisseurs :</strong> Privilégier les fournisseurs locaux pour réduire les distances de transport et ainsi diminuer notre empreinte carbone.</li>
+    </ul>
+
+    <p class="mt-4"><strong>Énergie Renouvelable et Efficacité Énergétique</strong></p>
+    <p class="mt-2">Nous investissons dans des solutions pour rendre nos installations plus respectueuses de l’environnement :</p>
+    <ul class="list-disc list-inside mt-2">
+      <li><strong>Énergies Renouvelables :</strong> Installation de panneaux solaires et recours à des fournisseurs d’énergie verte pour alimenter nos magasins et bureaux.</li>
+      <li><strong>Technologies Écoénergétiques :</strong> Mise en place de systèmes d’éclairage LED et de dispositifs de gestion de l’énergie pour réduire notre consommation.</li>
+    </ul>
+
+    <h3 class="text-xl font-semibold mt-6">Consommation Responsable</h3>
+    <p class="mt-4"><strong>Sensibilisation et Éducation</strong></p>
+    <p class="mt-2">Nous menons des campagnes de sensibilisation pour informer nos clients et employés sur l’importance de la consommation responsable :</p>
+    <ul class="list-disc list-inside mt-2">
+      <li><strong>Ateliers et Séminaires :</strong> Organisation de sessions éducatives sur les pratiques durables et les avantages de la consommation responsable.</li>
+      <li><strong>Matériel Informatif :</strong> Distribution de guides et de fiches pratiques pour aider nos clients à faire des choix plus écologiques.</li>
+    </ul>
+
+    <p class="mt-4"><strong>Promotion de Produits Durables</strong></p>
+    <p class="mt-2">Nous mettons en avant des produits qui respectent l’environnement et encouragent une consommation plus consciente :</p>
+    <ul class="list-disc list-inside mt-2">
+      <li><strong>Sélection de Produits Éco-responsables :</strong> Large gamme de produits écologiques disponibles dans nos magasins, allant des articles ménagers aux produits alimentaires.</li>
+      <li><strong>Labels et Certifications :</strong> Mise en avant des produits certifiés bio, équitables ou éco-responsables pour guider nos clients vers des choix durables.</li>
+    </ul>
+
+    <p class="mt-4"><strong>Partenariats Stratégiques</strong></p>
+    <p class="mt-2">Nous collaborons avec des organisations et des fournisseurs partageant notre engagement pour la durabilité :</p>
+    <ul class="list-disc list-inside mt-2">
+      <li><strong>Collaborations Écoresponsables :</strong> Travailler avec des partenaires qui s’engagent également dans des pratiques durables et éthiques.</li>
+      <li><strong>Soutien à des Initiatives Vertes :</strong> Soutenir des projets et des initiatives locales visant à améliorer la durabilité environnementale et sociale.</li>
+    </ul>
+
+    <h3 class="text-xl font-semibold mt-6">Engagement Continu</h3>
+    <p class="mt-4">Le Groupe Méré est déterminé à poursuivre et à renforcer ses démarches responsables. Nous sommes conscients que la route vers la durabilité est un processus continu qui nécessite innovation, engagement et collaboration. En intégrant ces pratiques dans notre culture d'entreprise, nous espérons non seulement réduire notre impact environnemental mais aussi inspirer d'autres à suivre le même chemin.</p>
+    
+    <p class="mt-4">Pour en savoir plus sur nos initiatives et comment vous pouvez y participer, n’hésitez pas à nous contacter ou à visiter l’une de nos boutiques. Ensemble, nous pouvons faire une différence positive pour notre planète.</p>
+  </div>
+</section>
+<?php get_footer(); ?>
---
+++ web/app/themes/pbTheme/page-notre-responsabilite-employeur.php
@@ -0,0 +1,45 @@
+<?php get_header(); ?>
+<section class="py-16 lg:min-w-[800px] mx-auto w-fit">
+  <h1 class="text-gray-800 text-xl font-extrabold sm:text-2xl text-center"><?php the_title() ?></h1>
+  <div class="text-gray-800">
+    <p class="mt-4">Chez le Groupe Méré, nous croyons fermement que nos employés sont notre plus grande richesse. Nous nous engageons à offrir un environnement de travail inclusif, sécurisé et stimulant. Nos politiques de ressources humaines sont conçues pour promouvoir l'égalité des chances, le développement professionnel et le bien-être de chaque membre de notre équipe.</p>
+
+    <h3 class="text-xl font-semibold mt-6">Environnement de Travail Inclusif</h3>
+    <p class="mt-4">Nous nous engageons à promouvoir la diversité et l'inclusion au sein de notre entreprise, en garantissant que chaque employé se sente valorisé et respecté. Voici quelques-unes de nos initiatives :</p>
+    <ul class="list-disc list-inside mt-2">
+      <li><strong>Programmes de Diversité et Inclusion :</strong> Création de programmes et d'ateliers pour sensibiliser et former notre personnel à l'importance de la diversité et de l'inclusion.</li>
+      <li><strong>Politiques Anti-discrimination :</strong> Mise en œuvre de politiques strictes contre toute forme de discrimination et de harcèlement sur le lieu de travail.</li>
+    </ul>
+
+    <h3 class="text-xl font-semibold mt-6">Développement Professionnel</h3>
+    <p class="mt-4">Nous offrons des programmes de formation continue et des opportunités de carrière pour permettre à nos employés de se développer et de progresser dans leur parcours professionnel. Nos initiatives incluent :</p>
+    <ul class="list-disc list-inside mt-2">
+      <li><strong>Formation Continue :</strong> Accès à des cours, des ateliers et des formations pour améliorer les compétences professionnelles et personnelles.</li>
+      <li><strong>Plans de Carrière Personnalisés :</strong> Élaboration de plans de carrière personnalisés pour aider chaque employé à atteindre ses objectifs professionnels.</li>
+    </ul>
+
+    <h3 class="text-xl font-semibold mt-6">Bien-être des Employés</h3>
+    <p class="mt-4">Nous mettons en place des initiatives visant à favoriser un équilibre sain entre vie professionnelle et vie personnelle, pour garantir le bien-être de nos employés. Cela comprend :</p>
+    <ul class="list-disc list-inside mt-2">
+      <li><strong>Programmes de Bien-être :</strong> Propositions de programmes de bien-être incluant des activités physiques, des ateliers de gestion du stress et des conseils de santé.</li>
+      <li><strong>Flexibilité du Travail :</strong> Possibilités de travail flexible et de télétravail pour aider nos employés à équilibrer leurs responsabilités professionnelles et personnelles.</li>
+    </ul>
+
+    <h3 class="text-xl font-semibold mt-6">Engagement Communautaire</h3>
+    <p class="mt-4">Nous encourageons nos employés à s'engager dans des activités bénévoles et communautaires. Le Groupe Méré soutient activement des projets locaux et encourage ses employés à participer à des initiatives caritatives :</p>
+    <ul class="list-disc list-inside mt-2">
+      <li><strong>Volontariat d'Entreprise :</strong> Organisation de journées de volontariat où les employés peuvent contribuer à des causes sociales et environnementales.</li>
+      <li><strong>Soutien aux Initiatives Locales :</strong> Partenariats avec des organisations locales pour soutenir des projets communautaires et de développement.</li>
+    </ul>
+
+    <h3 class="text-xl font-semibold mt-6">Reconnaissance et Récompense</h3>
+    <p class="mt-4">Nous reconnaissons et récompensons les contributions de nos employés à travers divers programmes de reconnaissance. Cela inclut :</p>
+    <ul class="list-disc list-inside mt-2">
+      <li><strong>Programmes de Reconnaissance :</strong> Mise en place de programmes de reconnaissance régulière pour célébrer les réalisations et les contributions exceptionnelles.</li>
+      <li><strong>Récompenses et Avantages :</strong> Offrir des récompenses et des avantages compétitifs pour motiver et fidéliser nos talents.</li>
+    </ul>
+
+    <p class="mt-4">Chez le Groupe Méré, nous sommes déterminés à créer un environnement de travail où chacun peut s'épanouir et contribuer pleinement. Nous croyons que notre succès repose sur le bien-être et le développement de nos employés, et nous nous engageons à continuer d'améliorer nos pratiques pour soutenir cette vision.</p>
+  </div>
+</section>
+<?php get_footer(); ?>
